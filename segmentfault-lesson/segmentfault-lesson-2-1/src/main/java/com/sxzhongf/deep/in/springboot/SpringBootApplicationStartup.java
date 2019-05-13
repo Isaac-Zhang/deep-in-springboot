7e5e901483638a9557167d607553e3eb4a503624
张盼 <pan.zhang@yi23.net>
2019-05-13 15:21:05 +0800

    SpringBoot REST STUDY

---
+++ segmentfault-lesson/segmentfault-lesson-2-1/src/main/java/com/sxzhongf/deep/in/springboot/SpringBootApplicationStartup.java
@@ -0,0 +1,19 @@
+package com.sxzhongf.deep.in.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBootApplicationStartup for study SpringBoot REST
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang</a>
+ * @see
+ * @since 2019/5/13
+ */
+@SpringBootApplication
+public class SpringBootApplicationStartup {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationStartup.class, args);
+    }
+}
